4eccd643acf7f5a04e6dbf2bd634868d29c1290c
mmustafademirhann <mmustafademirhann@gmail.com>
2025-04-11 00:16:58 +0300

    functional updates

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -21,6 +21,29 @@ const HomePage = () => {
       description: "En kaliteli ve lüks ürünlerimiz"
     }
   ];
+  const categories = [
+    {
+      id: 1,
+      title: "Kadın",
+      src: "https://picsum.photos/id/1012/400/400"
+    },
+    {
+      id: 2,
+      title: "Erkek",
+      src: "https://picsum.photos/id/1015/400/400"
+    },
+    {
+      id: 3,
+      title: "Çocuk",
+      src: "https://picsum.photos/id/1020/400/400"
+    },
+    {
+      id: 4,
+      title: "Aksesuar",
+      src: "https://picsum.photos/id/1035/400/400"
+    }
+  ];
+
 
   return (
     <>
@@ -28,18 +51,23 @@ const HomePage = () => {
       <Carousel images={carouselImages} />
 
       {/* Diğer içerikler ortalı ve sınırlı */}
-      <div className=" px-4">
+      <div className="max-w-7xl mx-auto px-4">
         {/* Kategoriler */}
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8 mt-8">
-          {["Kadın", "Erkek", "Çocuk", "Aksesuar"].map((category) => (
-            <div
-              key={category}
-              className="bg-gray-100 hover:bg-blue-100 rounded-lg p-6 text-center shadow cursor-pointer"
-            >
-              <h3 className="text-lg font-semibold">{category}</h3>
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-8 mt-8">
+          {categories.map((category) => (
+            <div key={category.id} className="relative cursor-pointer overflow-hidden group">
+              <img
+                src={category.src}
+                alt={category.title}
+                className="w-full h-[400px] object-cover"
+              />
+              <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white px-6 py-2 shadow text-black text-sm font-semibold tracking-wider uppercase">
+                {category.title}
+              </div>
             </div>
           ))}
         </div>
+
 
         {/* Kampanya Banner */}
         <div className="relative bg-blue-600 text-white text-center py-10 mb-8 rounded-lg shadow-lg">
