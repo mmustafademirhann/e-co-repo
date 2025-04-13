64bdfca18c786fcfbc3b2a9174255186ffb69512
mmustafademirhann <mmustafademirhann@gmail.com>
2025-04-13 12:53:42 +0300

    a

--- src/layout/ProductLayout.jsx
+++ src/layout/ProductLayout.jsx
@@ -2,7 +2,7 @@ import { Products } from "../components/Products";
 
 export const ProductLayout = ({ p }) => {
   return (
-    <div className="text-center my-12">
+    <div className="text-center my-12 px-4 sm:px-6 lg:px-20">
       <p className="text-sm text-gray-500">Featured Products</p>
       <h2 className="text-2xl sm:text-3xl font-bold mb-2">BESTSELLER PRODUCTS</h2>
       <p className="text-sm text-gray-500 mb-8">
